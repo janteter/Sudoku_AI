bb52ee9088f60a1fbc0aa774d0173d813d4bfc3d
jhucigit7544 <huangjk2@uci.edu>
2024-02-16 17:52:53 -0800

    Updates

--- batchtest.sh
+++ batchtest.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+echo "begining batch test"
+finalNum=5
+prefixStrArray=("easy" "intermed" "hard" "expert")
 
+for aprefixstr in ${prefixStrArray[@]};do
 
-python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_3.txt >outexp3.txt
-python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_4.txt >outexp4.txt
+    echo "Section: ${aprefixstr}"
+
+    for ((i=0;i<finalNum;i++));do
+        echo "On $i"
+        python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/${aprefixstr}_${i}.txt >out${aprefixstr}${i}.txt
+
+    done
+
+done
+
+# python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_0.txt >outexp0.txt
+# python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_1.txt >outexp1.txt
+# python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_2.txt >outexp2.txt
+# python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_3.txt >outexp3.txt
+# python3 Sudoku_Python_Shell/src/Main.py FC Sudoku_Generator/expert_4.txt >outexp4.txt
 
 echo "Done"
